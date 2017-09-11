564fadce63a574a73bf8038ba482835d06d367bf
Peggy Wang <peggy.yuchun.wang@gmail.com>
2017-09-11 11:23:28 -0500

    Video Resize and debug

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+ls -a ~
+export PATH=~/.local/bin:$PATH
+source ~/.bash_profile
 cd docs
 aws s3 cp index.html s3://footplusplus.com/
 aws s3 sync --delete . s3://footplusplus.com/
